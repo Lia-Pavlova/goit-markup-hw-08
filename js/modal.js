972571e0dece05829b9707b517c09dbc3f31980d
Natalia Pavlova <pavlova_li@ukr.net>
2021-05-08 05:25:21 +0300

    final

--- js/modal.js
+++ js/modal.js
@@ -9,9 +9,6 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
 
   function toggleModal() {
     document.body.classList.toggle("modal-open")
