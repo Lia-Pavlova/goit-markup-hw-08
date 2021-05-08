2938f7aad4ddaecae7f059d864776272bb779e4a
Natalia Pavlova <pavlova_li@ukr.net>
2021-05-08 04:06:11 +0300

    modal-icon-close

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,10 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+   function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+
   function toggleModal() {
     document.body.classList.toggle("modal-open")
     refs.modal.classList.toggle('is-hidden');
